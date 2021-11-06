eb57979da24c1a90057f680a8d8c4836ff679ced
Ruslanvalv <Ruslan161198@ya.ru>
2021-11-06 16:10:52 +0300

    commit message

---
+++ js/auth.js
@@ -0,0 +1,56 @@
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.getElementById('logInForm');
+
+const inputLogin = document.getElementById('login');
+const inputPass = document.getElementById('password');
+const buttonLogin = document.querySelector('.button-login');
+const buttonOut = document.querySelector('.button-out');
+const userName = document.querySelector('.user-name');
+
+const login = (user) => {
+  buttonAuth.style.display = 'none';
+  buttonOut.style.display = 'flex';
+  userName.style.display = 'flex';
+
+  userName.textContent = user.login;
+  closeForm();
+};
+
+const logOut = () => {
+  buttonAuth.style.display = 'flex';
+  buttonOut.style.display = 'none';
+  userName.style.display = 'none';
+  userName.textContent = '';
+  localStorage.removeItem('user');
+};
+
+const closeForm = () => {
+  modalAuth.style.display = 'none';
+};
+buttonAuth.addEventListener('click', () => {
+  modalAuth.style.display = 'flex';
+});
+
+buttonOut.addEventListener('click', () => {
+  logOut();
+});
+closeAuth.addEventListener('click', closeForm);
+logInForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const user = {
+    login: inputLogin.value,
+    password: inputPass.value,
+  };
+  console.log(user.login);
+  if (user.login) {
+    localStorage.setItem('user', JSON.stringify(user));
+    login(user);
+  } else {
+    alert('Вы не ввели логин.Пожалуйста введите логин.');
+  }
+});
+if (localStorage.getItem('user')) {
+  login(JSON.parse(localStorage.getItem('user')));
+}
---
+++ js/menu.js
@@ -0,0 +1,12 @@
+const restourant = 'food-band';
+
+const renderItems = (data) => {
+  data.forEach((element) => {
+    console.log(element);
+  });
+};
+
+fetch(`../db/${restourant}.json`)
+  .then((response) => response.json())
+  .then((data) => renderItems(data))
+  .catch((error) => console.log(error));
